4208a8ea8eceb199180adc15a309695e87a11468
jiangbin <bw0101@foxmail.com>
2024-02-09 16:26:25 +0800

    update

--- parse/qqwry/packer_gibo_xdb.js
+++ parse/qqwry/packer_gibo_xdb.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const libqqwry = require('lib-qqwry')
 const qqwry = libqqwry(true, './qqwry.dat')
 
-let gibo = new Map([
+let filter = new Map([
     ["CN", ["中国", "河北", "山西", "内蒙古", "辽宁", "吉林", "黑龙江", "江苏", "浙江", "安徽", "福建", "江西", "山东", "河南", "湖北", "湖南", "广东", "广西", "海南", "重庆", "四川", "贵州", "云南", "西藏", "陕西", "甘肃", "青海", "宁夏", "新疆", "香港", "澳门", "北京", "天津", "上海"]],
     ["US", ["美国"]]
 ])
@@ -22,18 +22,22 @@ let ip = '0.0.0.0'
 while (true) {
     let data = qqwry.searchIPScope(ip, ip)[0]
     if (data.Country !== 'IANA') {
-        let isOut = false
-        for (let [key, value] of gibo.entries()) {
+        let region = ""
+        for (let [key, value] of filter.entries()) {
             for (let str of value) {
                 if (data.Country.startsWith(str)) {
-                    isOut = true
+                    region = key
                     break
                 }
             }
-            if (isOut) {
-                outs.push(data.begIP + '|' + data.endIP + "|" + key)
+            if (region !== "") {
                 break
             }
+        }
+        if (region === "") {
+            outs.push("-")  // 忽略的ip段
+        } else {
+            outs.push(data.begIP + '|' + data.endIP + "|" + key)
         }
     }
     if (data.endIP === '255.255.255.255') break
