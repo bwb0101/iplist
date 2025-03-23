37559fc5c9b47f64be612609979fc59ccca82972
Ben <44266406@qq.com>
2025-03-23 10:49:50 +0800

    Update build.sh

--- parse/qqwry/build.sh
+++ parse/qqwry/build.sh
@@ -6,7 +6,7 @@ wget https://raw.githubusercontent.com/FW27623/qqwry/main/qqwry.dat
 npm install
 
 node packer.js
-node packer_xdb.js
+node packer_gibo_xdb.js
 ls -l
 chmod +x ./xdb_maker
-./xdb_maker gen --src=../data/data/ip.txt --dst=../data/data/ip.xdb
+./xdb_maker gen --src=../data/data/ip_gibo.txt --dst=../data/data/ip_gibo.xdb
